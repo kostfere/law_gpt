107a09ff6b8ff4dbf6db30347d65b32257651f96
kostfere <kostfere@gmail.com>
2024-01-15 01:38:45 +0000

    remove unused code

--- law_app.py
+++ law_app.py
@@ -11,33 +11,18 @@ from utils import (
     load_data,
 )
 
-import pinecone
 
-
-# Initialize Pinecone and OpenAI settings
-PINECONE_ENVIRONMENT = "gcp-starter"
-INDEX_NAME = "law-gpt"
-
-# Initialize pinecone
-
-pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_ENVIRONMENT)
-
-
-# Load data function
 @st.cache_data
 def load_data():
     data = pd.read_csv("processed_data.csv")
     return data
 
 
-# Load the data
 df = load_data()
 
-# Streamlit app setup
 st.title("Legal Cases Viewer")
 
 
-# Search functionality
 st.write("## Search for Similar Cases")
 search_query = st.text_input("Enter a search query:")
 top_n = st.slider("Select the number of top matches to display:", 1, 10, 3)
@@ -53,6 +38,7 @@ if st.button("Search"):
 st.write("## Generate Case Summary")
 case_id = st.selectbox("Select a Case ID for summary:", df["id"])
 show_original = st.checkbox("Show Original Text")
+
 
 if st.button("Generate Summary"):
     selected_case_text = df[df["id"] == case_id]["text"].iloc[0]
